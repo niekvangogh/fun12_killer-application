75c70ec304fff88f5fffba1e3a0b04f8a812a26d
Niek <niek@keersop.nl>
2019-01-06 19:54:25 +0100

    init commit

---
+++ killer app/Database.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace killer_app
+{
+    public class Database
+    {
+
+        public static Database INSTANCE { get; set; }
+
+        private Database()
+        {
+            INSTANCE = this;
+        }
+
+        public static Database GetInstance()
+        {
+            if (INSTANCE == null)
+            {
+                INSTANCE = new Database();
+            }
+            return INSTANCE;
+        }
+    }
+}
